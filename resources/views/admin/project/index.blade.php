64f51aa93fae83a6d36b0aea676d7df5534009b1
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-21 18:42:02 +0100

    Add - project index content. navbar link for projects Fix - index route in project controller.

---
+++ resources/views/admin/project/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="py-4">
+        <div class="container">
+            <h1>Projects</h1>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="d-grid" style="grid-template-columns: repeat(3,1fr); gap: 20px;">
+                @forelse ($projects as $project)
+                    <div class="card">
+                        <img src="{{ $project->img}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $project->title }}</h5>
+                        <p class="card-text">{{ $project->description }}</p>
+                        <a href="#" class="btn btn-primary">Edit</a>
+                        <a href="#" class="btn btn-danger">Delete</a>
+                    </div>
+                    </div>
+                @empty
+                    No Projects to display
+                @endforelse
+            </div>
+        </div>
+    </section>
+    
+@endsection
