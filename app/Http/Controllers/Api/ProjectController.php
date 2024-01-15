b1ce177397e9559086d0d243ca94c643405c6092
FrancescoDOrtona <francescod.97@hotmail.it>
2024-01-15 16:47:00 +0100

    Add: Api/ProjectController content

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index( Request $request ){
+        $results = Project::with('type')->paginate(20);
+        return response()->json([
+            'results' => $results,
+            'success' => true
+        ]);
+    }
 }
